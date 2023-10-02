8f8976786ffb61a7714335150dc745953d62fc30
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2023-10-02 14:08:27 +0100

    Completed logic and input for password generation

--- main.py
+++ main.py
@@ -37,4 +37,22 @@ while True:
 part1 = round(characters_number * (30/100))
 part2 = round(characters_number * (20/100))
 
+# Generate password (60% letters and 40% digits and punctuation)
+result = []
+
+for x in range(part1):
+    result.append(s1[x])
+    result.append(s2[x])
+
+for x in range(part2):
+    result.append(s3[x])
+    result.append(s4[x])
+
+
+# Shuffle the result
+random.shuffle(result)
+
+# Join result
+password = "".join(result)
+print("Strong password: ", password)
 
